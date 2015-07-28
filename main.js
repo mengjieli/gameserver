9bb13bb4f9d12c1b6231956c9db412436db8e812
limengjie <duoduo520>
2015-07-28 18:00:42 +0800

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+var http = require("http");
+
+http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    response.write("Hello World");
+
+    response.end();
+
+    console.log("a request");
+
+}).listen(8888);
