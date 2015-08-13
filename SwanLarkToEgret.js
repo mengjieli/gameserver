03f59d6616be699dd5f7e3c701cefcf2459967eb
limengjie <duoduo520>
2015-08-13 12:06:33 +0800

    no message

---
+++ SwanLarkToEgret.js
@@ -0,0 +1,20 @@
+var File = require("./File.js");
+var path = require("path");
+var swanFile = path.resolve(process.cwd(), decodeURI(process.argv[2]));
+var outFile = path.resolve(process.cwd(), decodeURI(process.argv[3]));
+global.filedev = "/";
+
+var files = global.File.readDir(swanFile,function (file) { return /.ts/.test(file); });
+
+for(var i = 0,len = files.length; i < len; i++)
+{
+    var file = global.File.readUTF8File(swanFile + "/" + files[i]);
+    global.File.addFileList([2,outFile + "/" + files[i],file,"utf-8"]);
+}
+
+global.File.setFileListComplete(function()
+{
+    console.log("拷贝完成");
+});
+
+global.File.copyNextFile();
